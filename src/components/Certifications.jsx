9edd62d8b5fe6ab5c12a09c8632a82e7a7de0e10
Ulysses Uribe <ulysses.uribe@gmail.com>
2024-04-02 17:37:02 -0700

    Update Certifications.jsx
    update certifications

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SiCredly, SiFreecodecamp } from "react-icons/si";
+import { SiCredly, SiFreecodecamp, SiEdx } from "react-icons/si";
 
 const Certifications = () => {
   return (
@@ -7,6 +7,10 @@ const Certifications = () => {
         <div className='max-w-[1240px] w-4/5 lg:w-2/3 h-full py-12 m-auto flex flex-col'>
             <h2 className='tracking-widest text-blue-800 text-left'>Certifications</h2>
             <div className='mt-4 p-4 shadow-lg rounded-lg lg:pr-32 xl:pr-80'>
+                <a href="https://tinyurl.com/5cckr39u ">
+                    <div className='pt-4 flex flex-row underline cursor-pointer hover:font-bold hover:scale-105 ease-in duration-100 items-center justify-between'> EdX Prompt Engineering and Advanced ChatGPT 2024 <SiEdx  size={28} />
+                    </div>  
+                </a>
                 <a href="https://www.credly.com/badges/9dfc41a1-4302-455f-859a-86a3b5f96bf6/public_url ">
                     <div className='pt-4 flex flex-row underline cursor-pointer hover:font-bold hover:scale-105 ease-in duration-100 items-center justify-between'> IBM Web Development Fundamentals 2023 <SiCredly size={28} />
                     </div>  
